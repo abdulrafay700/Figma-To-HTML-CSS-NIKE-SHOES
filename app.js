651f880f9233c5dc6c5eed8fd2d131a98b8ef525
unknown <abdulrafay700@gmail.com>
2024-01-20 22:57:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// let button = document.querySelector("input[type=checkbox]");
+// let burger = document.querySelector(".navbar-toggler-icon")
+
+// button.addEventListener('click', function() {
+//     console.log('Button clicked');
+//     if (button.checked){
+//         burger.classList.add("display-none")
+//     }
+//     else{
+//         burger.classList.add("display-block")
+//     }
+// });
+
